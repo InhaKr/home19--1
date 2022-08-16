ec93a6973f5633907297f219b3dc2d09d587121e
InhaKr <inkr3891@gmail.com>
2022-08-16 17:41:02 +0300

    19--1

---
+++ js/main.js
@@ -0,0 +1,23 @@
+  "use strict";
+  var a;
+
+  function x() {
+    var result;
+    var num1 = Number(document.getElementById("num1").value);
+    var num2 = Number(document.getElementById("num2").value);
+    switch (a) {
+      case '+':
+        result = num1 + num2;
+        break;
+      case '-':
+        result = num1 - num2;
+        break;
+      case '*':
+        result = num1 * num2;
+        break;
+      case '/':
+        result = num1 / num2;
+        break;
+    }
+    document.getElementById("result").innerHTML = result;
+  }
